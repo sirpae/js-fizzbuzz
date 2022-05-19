62e2baef5a31be6051c8048ce32cb3591dfe342a
p <101421945+sirpae@users.noreply.github.com>
2022-05-19 21:08:59 +0100

    Completed

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,23 @@
 // TODO: Write your function in this file
+function fizzBuzz (lower, upper) {
+    const outputArray = []
+  
+    for (let i = lower; i <= upper; i++) {
+      const number = i
+  
+      if (number % 3 === 0 && number % 5 === 0) {
+        outputArray.push('FizzBuzz')
+      } else if (number % 3 === 0) {
+        outputArray.push('Fizz')
+      } else if (number % 5 === 0) {
+        outputArray.push('Buzz')
+      } else {
+        outputArray.push(number)
+      }
+    }
+  
+    return outputArray
+  }
 
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzBuzz
